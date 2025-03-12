6f3291a84928a06af6d77facd93fe13865254500
nichnikov <a.nichnikov@gmail.com>
2025-03-12 14:09:48 +0300

    new data

--- data/mss_by_date.sql
+++ data/mss_by_date.sql
@@ -1,4 +1,3 @@
-select * from online_helper__chats.messages m
-LIMIT 100
-/*WHERE  m.created >= '2025-2-24'::date AND m.created =< '2025-3-2'::date  AND m. not like '%AUTOTEST%'
+select * from online_helper__chats.chats c
+WHERE  c.created >= '2025-2-24'::date AND c.created <= '2025-3-2'::date
 
