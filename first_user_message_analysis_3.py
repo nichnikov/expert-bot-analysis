6e888a6c9dae6e366e3614e35e3dec95eb9b3501
nichnikov <a.nichnikov@gmail.com>
2025-03-20 20:11:19 +0300

    ref

--- first_user_message_analysis_3.py
+++ first_user_message_analysis_3.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pandas as pd
 from core.data_definition import Parameters, Settings
 from services.validator import GPT_Validator
@@ -18,6 +19,12 @@ parameters.prompt = """
         Вопрос пользователя: {}
 """
 
+# поля в данных:
+'''
+    "FirstPhrase"
+    "chat_id"
+    "len_w"'''
+
 validator = GPT_Validator(settings, parameters)
 data_path = os.path.join("data", "first_user_message_analysis", "bss_user_first_phrase_20250201_20250320_3_and_more.feather")
 data_df = pd.read_feather(data_path)
@@ -26,11 +33,11 @@ k = 1
 for d in data_dicts:
     phrase = d["FirstPhrase"]
     val = validator.gpt_validation(phrase)
-    print(k, phrase, "\n", val, "\n\n" )
-    '''
-    "FirstPhrase"
-    "chat_id"
-    "len_w"'''
+    answer_num = re.findall("[12345]", val)
+    
+    print(k, phrase, "\n", val, "\n", answer_num, "\n\n" )
+    
+
     k += 1
     if k > 100:
         break
